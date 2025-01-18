7ef80b4c988ee39f1332e0899e56cd6cb2a52cc8
ShkalikovAV <shkalikov@atb.su>
2025-01-19 01:59:06 +0300

    добавлен шаблон в задачи \ рефактор

--- code.js
+++ code.js
@@ -16,6 +16,7 @@ const borderWidth = 4;
 const borderColor = { r: 122 / 255, g: 135 / 255, b: 255 / 255 }; // #7A87FF
 const backgroundColor = { r: 68 / 255, g: 68 / 255, b: 68 / 255 }; // #444444
 const pageBackgroundColor = { r: 30 / 255, g: 30 / 255, b: 30 / 255 }; // #1E1E1E
+const rectangleTemplate = { r: 217 / 255, g: 217 / 255, b: 217 / 255 };
 const sectionTitles = ["Задача", "Процесс", "Результат"];
 // Создание секции
 function createSection(x, y, title) {
@@ -26,6 +27,24 @@ function createSection(x, y, title) {
         section.y = y;
         section.name = title; // добавляем название секции
         figma.currentPage.appendChild(section); // добавляем секцию на страницу
+        // Создаем прямоугольник в первой секции с отступами
+        if (title === sectionTitles[0]) {
+            const rectangle = figma.createRectangle();
+            rectangle.resize(sectionWidth - 150, 500); // ширина с учетом отступов (75px с каждой стороны)
+            rectangle.x = x + 75; // отступ слева
+            rectangle.y = y + 75; // отступ сверху
+            rectangle.fills = [{ type: 'SOLID', color: rectangleTemplate }]; // цвет фона прямоугольника
+            section.appendChild(rectangle); // добавляем прямоугольник в секцию
+            // Загружаем шрифт перед созданием текстового элемента
+            yield figma.loadFontAsync({ family: "Inter", style: "Regular" });
+            const text = figma.createText();
+            text.fontName = { family: "Inter", style: "Regular" };
+            text.characters = "Шаблон текста";
+            text.x = rectangle.x; // отступ слева
+            text.y = rectangle.height + 125; // отступ сверху
+            text.fontSize = 36;
+            text.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
+        }
         return section;
     });
 }
@@ -34,9 +53,11 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         // Устанавливаем цвет фона страницы
         figma.currentPage.backgrounds = [{ type: 'SOLID', color: pageBackgroundColor }];
-        // Вычисляем начальные координаты для размещения секций в центре страницы
-        const startX = figma.viewport.bounds.x;
-        const startY = figma.viewport.bounds.y;
+        // Вычисляем начальные координаты viewport'а
+        const startX = 0;
+        const startY = 0;
+        // const startX = figma.viewport.bounds.x;
+        // const startY = figma.viewport.bounds.y;
         // Массив для хранения созданных секций
         const createdSections = [];
         // Создание секций
@@ -48,6 +69,7 @@ function main() {
         }
         figma.currentPage.selection = createdSections; // выделяет созданные секции
         figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection); // подгоняет масштаб под выделенные объекты
+        figma.currentPage.selection = [];
         figma.closePlugin();
     });
 }
--- code.ts
+++ code.ts
@@ -6,6 +6,7 @@ const borderWidth = 4;
 const borderColor = { r: 122 / 255, g: 135 / 255, b: 255 / 255 }; // #7A87FF
 const backgroundColor = { r: 68 / 255, g: 68 / 255, b: 68 / 255 }; // #444444
 const pageBackgroundColor = { r: 30 / 255, g: 30 / 255, b: 30 / 255 }; // #1E1E1E
+const rectangleTemplate = { r: 217 / 255, g: 217 / 255, b: 217 / 255 };
 const sectionTitles = ["Задача", "Процесс", "Результат"];
 
 // Создание секции
@@ -16,6 +17,28 @@ async function createSection(x: number, y: number, title: string) {
     section.y = y;
     section.name = title; // добавляем название секции
     figma.currentPage.appendChild(section); // добавляем секцию на страницу
+
+        // Создаем прямоугольник в первой секции с отступами
+        if (title === sectionTitles[0]) {
+          const rectangle = figma.createRectangle();
+          rectangle.resize(sectionWidth - 150, 500); // ширина с учетом отступов (75px с каждой стороны)
+          rectangle.x = x + 75; // отступ слева
+          rectangle.y = y + 75; // отступ сверху
+          rectangle.fills = [{ type: 'SOLID', color: rectangleTemplate }]; // цвет фона прямоугольника
+          
+          section.appendChild(rectangle); // добавляем прямоугольник в секцию
+
+          // Загружаем шрифт перед созданием текстового элемента
+          await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+          const text = figma.createText();
+          text.fontName = { family: "Inter", style: "Regular" };
+          text.characters = "Шаблон текста"
+          text.x = rectangle.x; // отступ слева
+          text.y = rectangle.height + 125; // отступ сверху
+          text.fontSize = 36;
+          text.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
+      }
+
     return section;
 }
 
@@ -25,8 +48,10 @@ async function main() {
   figma.currentPage.backgrounds = [{ type: 'SOLID', color: pageBackgroundColor }];
 
   // Вычисляем начальные координаты viewport'а
-  const startX = figma.viewport.bounds.x;
-  const startY = figma.viewport.bounds.y;
+  const startX = 0;
+  const startY = 0;
+  // const startX = figma.viewport.bounds.x;
+  // const startY = figma.viewport.bounds.y;
 
   // Массив для хранения созданных секций
   const createdSections = []; 
@@ -41,7 +66,7 @@ async function main() {
 
   figma.currentPage.selection = createdSections; // выделяет созданные секции
   figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection) // подгоняет масштаб под выделенные объекты
-
+  figma.currentPage.selection = []
   figma.closePlugin();
 }
 
